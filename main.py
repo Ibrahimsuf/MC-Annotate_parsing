7c1ac13533d18c3c15361ca2ef8ad51063176dea
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2023-10-15 07:09:41 -0500

    output basepair annotations from mcannotate and rnaview into 1 csv

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ def main():
     rna_file = sys.argv[1]
 
     rna = RNA(rna_file)
-    rna.write_to_output_file("base_pairs")
+    rna.run_mc_annotate()
+    rna.run_RNAView()
+    rna.get_MC_base_pair_annotations()
+    rna.get_RNAVIEW_annotations()
+    rna.write_base_pair_annotations_to_file()
+
+    
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
--- rna.py
+++ rna.py
@@ -1,19 +1,20 @@
 import subprocess
 from Bassepairannotation import BasePairAnnotation
-
+import pandas as pd
 class RNA:
     def __init__(self, pdb_file) -> None:
         self.name = pdb_file.split(".")[0]
         self.pdb_file = pdb_file
         self.MCAnnotate_output = None
         self.RNAVIEW_output = None
-        self.MC_base_pair_Annotations = {}
-        self.RNAVIEW_base_pair_Annotations = {}
+        self.MC_base_pair_annotations = {}
+        self.RNAVIEW_base_pair_annotations = {}
+        self.annotations = {}
+        self.annotations_unpacked = {}
 
     def run_mc_annotate(self):
         command = ["./MC-Annotate", self.pdb_file]
         try:
-            # Run the command and capture the output
             result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
 
             if result.stderr:
@@ -28,20 +29,56 @@ class RNA:
     
     def run_RNAView(self):
         command = ["rnaview", self.pdb_file]
+        try:
+            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+
+            if result.stderr:
+                print("Error output:")
+                print(result.stderr)
+            else:
+                output_filename = f"{self.pdb_file}.out"
+                self.RNAVIEW_output =  get_file_contents(output_filename)
+
+            print("RNA View completed successfully.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error running MC-Annotate: {e}")
+
     
     def get_MC_base_pair_annotations(self):
-        base_pair_info = get_lines_after(self.MC_base_pair_Annotations, "Base-pairs")
+        base_pair_info = get_lines_after(self.MCAnnotate_output, "Base-pairs")
 
         for line in base_pair_info:
             #create an annotation object for each base_pair
             base_pair_annotation = BasePairAnnotation()
             base_pair_annotation.read_features_from_MCAnnotate_ouput(line)
-            self.MC_base_pair_Annotations[base_pair_annotation.location] = base_pair_annotation
+
+            if base_pair_annotation.MClocation in self.annotations:
+                self.annotations[base_pair_annotation.MClocation] = self.annotations[base_pair_annotation.MClocation].merge(base_pair_annotation)
+            else:
+                self.annotations[base_pair_annotation.MClocation] = base_pair_annotation
     
     def get_RNAVIEW_annotations(self):
+        base_pair_info = get_lines_after(self.RNAVIEW_output, "BEGIN_base-pair", "END_base-pair")
+        for line in base_pair_info:
+            base_pair_annotation = BasePairAnnotation()
+            base_pair_annotation.read_features_from_RNAVIEW_ouput(line)
+
+            if base_pair_annotation.RNAVIEW_location in self.annotations:
+                self.annotations[base_pair_annotation.RNAVIEW_location].merge(base_pair_annotation)
+            else:
+                self.annotations[base_pair_annotation.RNAVIEW_location] = base_pair_annotation
+    
+    def write_base_pair_annotations_to_file(self):
+        for key, annotation in self.annotations.items():
+            self.annotations_unpacked[key] = annotation.features
+
+        output_file_name = f"{self.name}.all_base_pair_annotations"
+        pd.DataFrame(self.annotations_unpacked).to_csv(output_file_name)
+        
         
 
-def get_lines_after(lines, heading_keyword):
+
+def get_lines_after(lines, heading_keyword, ending_keyword = None):
         lines_after_heading = []
         heading_found = False
 
@@ -50,11 +87,18 @@ def get_lines_after(lines, heading_keyword):
             if heading_keyword in line:
                 heading_found = True
                 continue
+
+            if ending_keyword and ending_keyword in line:
+                break
             # If the heading has been found, add the line to the result
             if heading_found:
                 lines_after_heading.append(line)
 
         return lines_after_heading
+
+def get_file_contents(file_name):
+    with open(file_name, "r"):
+        return file_name.read()
 
 
 
